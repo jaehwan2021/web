7d3d1442241ada643f6917765a0d01dcf696ebe6
jaehwan2021 <gjhh1588@gmail.com>
2021-02-14 16:36:24 +0900

    Update stock_code_test.py
    주석처리

--- stock_code_test.py
+++ stock_code_test.py
@@ -6,13 +6,25 @@ import matplotlib.pyplot as plt
 import datetime
 
 
-#url 접속할 수 있는 기본코드
-
+#url 기반 웹 크롤링
 url = 'https://www.investing.com/equities/jp-morgan-chase-historical-data'
+
+#서버 접속간 사용자 고유 코드 (구글에 what is my user agent 검색 및 복붙해서 사용할 것)
 headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.146 Safari/537.36"}
+
+#해당 url을 user-agent 정보를 통해 가져옴
 req = requests.get(url, headers=headers)
+
+# req가 비정상적으로 가져와질 경우 code종료
 req.raise_for_status()
-soup = BeautifulSoup(req.text, 'lxml')
+
+#req해온 text를 파싱해서 soup란 객체에 저장
+soup = BeautifulSoup(req.text, 'html.parser')
+
+
+
+
+#작업중
 
 time = input()
 
