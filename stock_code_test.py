8e18ecdd7819121aa42bdc3b34aacdb3d7a71536
Deeny-Seo <dhs1463@gmail.com>
2021-02-24 21:22:26 +0900

    특정날짜 지정을 위한 작업중
    datatime 추가.
    특정 날짜 입력.
    특정 날짜 입력 후 해당 날짜를 가져오게 수정.
    추후 1년단위로 가져오고, 가져올 데이터가 0개일때까지 반복하여 상장된 날부터의 데이터를 취합 할 예정.

--- stock_code_test.py
+++ stock_code_test.py
@@ -4,6 +4,7 @@ import requests
 import pandas as pd
 import matplotlib.pyplot as plt
 import time
+import datetime
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -27,8 +28,8 @@ def save(file_name):
         conn.close()
 
 # https://www.investing.com/equities/ + "variable" + -historical-data 형태 url의 파싱한 data를 return하는 함수로 정의
-def souping(company_name):
-
+def souping(company_name1):
+company_name = "apple-computer-inc"
     #입력받은 변수를 함수정의에 사용하기 위해 임의의 변수에 저장
     data_save_company_name = company_name
 
@@ -40,32 +41,50 @@ def souping(company_name):
     headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36 Edg/88.0.705.68"}
 
     #해당 url을 user-agent 정보를 통해 가져옴
-    req = requests.get(url, headers=headers)
+
+   #d = webdriver.Chrome(executable_path='C:\project\chromedriver')
+    d = webdriver.Chrome(executable_path='C:\project\chromedriver')
+    d.get(url)
+    d.implicitly_wait(5)
+    NoErro=False
+    while NoErro==False:
+        try:  # attempt to dismiss banners that could block later clicks
+            print("Start")
+            d.find_element_by_id('widgetFieldDateRange').click()  # show the date picker
+            sDate = d.find_element_by_id('startDate')  # set start date input element into variable
+            sDate.clear()  # clear existing entry
+            sDate.send_keys('01/18/2019')  # add custom entry
+            eDate = d.find_element_by_id('endDate')  # repeat for end date
+            eDate.clear()
+            eDate.send_keys('01/18/2020')
+            d.find_element_by_id('applyBtn').click()  # submit changes
+            NoErro=True
+            print("End")
+        except:
+            print("Except Start")
+            NoErro = False
+            d.implicitly_wait(5)
+            d.get(url)
+            print("Except End")
+
+
+        #WebDriverWait(d, 10).until(EC.element_to_be_clickable(By.CSS_SELECTOR, ".popupCloseIcon largeBannerCloser")).click()
+        datatest = EC.element_to_be_clickable((By.CSS_SELECTOR, ".popupCloseIcon largeBannerCloser"))
+        ##EC.element_to_be_clickable((By.CSS_SELECTOR, ".popupCloseIcon largeBannerCloser")).click()
+        #datatest.click()
+        #d.find_element_by_css_selector('.closer').click()
+        #print(d.find_element_by_class_name('.popupCloseIcon largeBannerCloser'))
+
+
+
+
+   req = d.page_source
 
     # req가 비정상적으로 가져와질 경우 code종료
     req.raise_for_status()
 
-   #d = webdriver.Chrome(executable_path='C:\project\chromedriver')
-    d = webdriver.Chrome(executable_path='C:\project\chromedriver')
-    d.get('https://www.investing.com/funds/lansforsakringar-global-indexnara-historical-data')
-    try:  # attempt to dismiss banners that could block later clicks
-        WebDriverWait(d, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".closer"))).click()
-        d.find_element_by_css_selector('.closer').click()
-    except:
-        pass
-    d.find_element_by_id('widgetFieldDateRange').click()  # show the date picker
-    sDate = d.find_element_by_id('startDate')  # set start date input element into variable
-    sDate.clear()  # clear existing entry
-    sDate.send_keys('01/18/2019')  # add custom entry
-    eDate = d.find_element_by_id('endDate')  # repeat for end date
-    eDate.clear()
-    eDate.send_keys('01/28/2019')
-    d.find_element_by_id('applyBtn').click()  # submit changes
-
-
-
     #req해온 text를 파싱해서 soup란 객체에 저장
-    soup = BeautifulSoup(req.text, 'html.parser')
+    soup = BeautifulSoup(req, 'html.parser')
 
     return soup
 
@@ -75,9 +94,11 @@ def souping(company_name):
 soup = souping("apple-computer-inc")
 
 # 현재 시간을 초 단위로 나타냄 // 86400을 통해 일 단위로 변환
-time = int(time.time() // 86400)
+time_now = int(time.time() // 86400)
 
-
+mytime = datetime.datetime(2020, 1, 18, 0, 0, 0,0)
+mytimeNumber = int(time.mktime(mytime.timetuple())//86400)
+print(mytimeNumber)
 # 1613288385
 # 1613088000 / 86400 -> 18670일 -> 2월 14일 //하루는 86400초
 soup.find("picker")
@@ -87,7 +108,7 @@ print(soup.text)
 for i in range(40):
     # 날짜에 따른 종가 출력
     try:
-        date = soup.find("td", attrs={'data-real-value':'{}'.format(time*86400 - (i*86400))})
+        date = soup.find("td", attrs={'data-real-value':'{}'.format(mytimeNumber*86400 - (i*86400))})
         td_price = date.find_next_sibling("td")
         td_open = td_price.find_next_sibling("td")
         td_high = td_open.find_next_sibling("td")
