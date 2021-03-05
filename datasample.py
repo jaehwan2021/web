c78aa7eb98bfe0fa24bb87b3f0adb90513a69ba1
jaehwan2021 <gjhh1588@gmail.com>
2021-03-06 07:29:14 +0900

    작업중... 저장
    작업중... 저장

--- datasample.py
+++ datasample.py
@@ -12,7 +12,10 @@ import time
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import tensorflow as tf
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
+
+# -outputs, states = tf.contrib.rnn.static_rnn(lstm_cells, _X, dtype=tf.float32)
 
 yf.pdr_override()
 # tf.set_random_seed(777)
@@ -34,6 +37,10 @@ def reverse_min_max_scaling(org_x, x):  # 종가 예측값
     org_x_np = np.asarray(org_x)
     x_np = np.asarray(x)
     return (x_np * (org_x_np.max() - org_x_np.min() + 1e-7)) + org_x_np.min()
+
+
+
+
 
 input_dcm_cnt = 10 #입력데이터의 컬럼 개수
 output_dcm_cnt = 1 #결과데이터의 컬럼 개수
@@ -97,3 +104,75 @@ targets = tf.placeholder(tf.float32, [None, 1])
 predictions = tf.placeholder(tf.float32, [None, 1])
 print("targets", targets)
 print("predictions", predictions)
+
+def lstm_cell():
+    cell = tf.contrib.rnn.BasicLSTMCell(num_units=rnn_cell_hidden_dim,
+                                       forget_bias=forget_bias,
+                                       state_is_tuple=True,
+                                       activation=tf.nn.softsign)
+    if keep_prob < 1.0 :
+        cell = tf.contrib.rnn.DropoutWrapper(cell, output_keep_prob=keep_prob)
+    return cell
+
+
+stackedRNNs = [lstm_cell()for _ in range(num_stacked_layers)] #Stacked LSTM Layers 개수 1
+multi_cells = tf.contrib.rnn.MultiRNNCell(stackedRNNs, state_is_tuple=True)if num_stacked_layers > 1 else lstm_cell()
+
+hypothesis, _states = tf.nn.dynamic_rnn(multi_cells, X, dtype=tf.float32)
+hypothesis = tf.contrib.layers.fully_connected(hypothesis[:,-1], output_dcm_cnt, activation_fn=tf.identity)
+hypothesis.shape
+
+loss = tf.reduce_sum(tf.square(hypothesis - Y))
+optimizer = tf.train.AdamOptimizer(learning_rate)
+train = optimizer.minimize(loss)
+
+rmse = tf.sqrt(tf.reduce_mean(tf.squared_difference(targets, predictions)))
+
+train_error_summary = []
+test_error_summary = []
+test_predict = ''
+
+sess = tf.Session()
+sess.run(tf.global_variables_initializer())
+
+#학습
+start_time = datetime.datetime.now()
+print('학습 시작...')
+
+for epoch in range(epoch_num):
+    _, _loss = sess.run([train, loss], feed_dict={X: trainX, Y: trainY})
+    if ((epoch + 1) % 100 == 0) or (epoch == epoch_num - 1):  # 100번째마다 또는 마지막 epoch인 경우
+        # 학습용데이터로 rmse오차를 구한다
+        train_predict = sess.run(hypothesis, feed_dict={X: trainX})
+        train_error = sess.run(rmse, feed_dict={targets: trainY, predictions: train_predict})
+        train_error_summary.append(train_error)
+
+        # 테스트용데이터로 rmse오차를 구한다
+        test_predict = sess.run(hypothesis, feed_dict={X: testX})
+        test_error = sess.run(rmse, feed_dict={targets: testY, predictions: test_predict})
+        test_error_summary.append(test_error)
+        print("epoch: {}, train_error(A): {}, test_error(B): {}, B-A: {}".format(epoch + 1, train_error, test_error,
+                                                                                 test_error - train_error))
+
+end_time = datetime.datetime.now() #종료시간을 기록
+elapsed_time = end_time - start_time # 경과시간을 구한다
+print('elapsed_time:',elapsed_time)
+print('elapsed_time per epoch:',elapsed_time/epoch_num)
+
+print(',train_error:', train_error_summary[-1], end='')
+print(',test_error:', test_error_summary[-1], end='')
+print(',min_test_error:', np.min(test_error_summary))
+
+# 결과 그래프 출력
+plt.figure(1)
+plt.plot(train_error_summary, 'gold')
+plt.plot(test_error_summary, 'b')
+plt.xlabel('Epoch(x100)')
+plt.ylabel('Root Mean Square Error')
+
+plt.figure(2)
+plt.plot(testY, 'r')
+plt.plot(test_predict, 'b')
+plt.xlabel('Time Period')
+plt.ylabel('Stock Price')
+plt.show()
